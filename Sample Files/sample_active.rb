bb37d4a0e5a3197c350427961e0c375e394ebb4d
Jay Dihenkar <jkdihenkar@gmail.com>
2015-07-17 10:43:13 +0530

    Original Fetch from Shieldsquare.com

---
+++ Sample Files/sample_active.rb
@@ -0,0 +1,44 @@
+#! /usr/bin/ruby
+
+require './ss2'
+shieldsquare_userid = "" # Enter the UserID of the user
+shieldsquare_calltype = 1
+shieldsquare_pid = ""
+shieldsquare_response=shieldsquare_ValidateRequest shieldsquare_userid, shieldsquare_calltype, shieldsquare_pid
+
+puts "Content-type: text/html"
+puts ''
+puts '<html>'
+puts '<head>'
+puts '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
+puts '<meta http-equiv="Content-Language" content="en" />'
+puts '<title>Sample Home Page</title>'
+puts '</head>'
+puts '<body>'
+if shieldsquare_response["responsecode"] == 0
+	puts "Allow the user request"
+elsif shieldsquare_response["responsecode"] == 1
+	puts "Monitor this Traffic"
+elsif shieldsquare_response["responsecode"] == 2
+	puts "Show CAPTCHA before displaying the content"
+elsif shieldsquare_response["responsecode"] == 3
+	puts "Block This request"
+elsif shieldsquare_response["responsecode"] == 4
+	puts "Feed Fake Data"
+elsif shieldsquare_response["responsecode"] == -1
+	puts "Curl Error - " + shieldsquare_response["reason"] + "<BR>"
+	puts "Please reach out to ShieldSquare support team for assistance <BR>"
+	puts "Allow the user request"
+end
+puts '<script type="text/javascript">'
+puts 'var __uzdbm_a = "' + shieldsquare_response["pid"] +'"'
+puts 'var __uzdbm_b = "' + shieldsquare_response["url"] +'"'
+puts shieldsquare_response["dynamic_JS"]
+puts '</script>'
+puts '<script type="text/javascript" src="http://cdn.perfdrive.com/static/browser_detect_min.js"></script>'
+puts '<h1>'
+puts '<BR>YOUR CODE GOES HERE'
+puts '</h1>'
+puts '</body>'
+puts '</html>'
+
